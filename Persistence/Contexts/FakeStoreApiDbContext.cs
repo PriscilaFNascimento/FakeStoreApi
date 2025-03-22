45001aaafb68d27439c56db4ae0b0ce266e90469
Priscila F. Nascimento <fnas.priscila@gmail.com>
2025-03-22 12:05:39 -0300

    feat: configuring FakeStoreApiDbContext (#4)

---
+++ Persistence/Contexts/FakeStoreApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Contexts
+{
+    public class FakeStoreApiDbContext : DbContext
+    {
+        public FakeStoreApiDbContext(DbContextOptions<FakeStoreApiDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FakeStoreApiDbContext).Assembly);
+        }
+    }
+}
---
+++ Persistence/Extensions/ServiceRegistrationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+
+namespace Persistence.Extensions
+{
+    public static class ServiceRegistrationExtensions
+    {
+        public static IServiceCollection AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FakeStoreApiDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(FakeStoreApiDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
